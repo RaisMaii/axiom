500ca3063daa4657845dc7c5e4e6514a2f0d0754
RaisMaii <121684912+RaisMaii@users.noreply.github.com>
2023-05-26 02:45:49 -0400

    Update script.js

--- client/script.js
+++ client/script.js
@@ -75,7 +75,9 @@ const messageDiv = document.getElementById(uniqueId);
 loader (messageDiv);
 //fetch data from server -> bot's response
 
-const response= await fetch('http://localhost:5000', {
+const response= await fetch('https://axiom-qusx.onrender.com
+
+', {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
